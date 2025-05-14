e3553b185eba75ed77780026dc5eb448fd5bd63a
ayushpandey157 <ayushpandey1572003@gmail.com>
2025-05-14 23:33:53 +0530

    Time: 37 ms (82.91%) | Memory: 81.6 MB (79.48%) - LeetSync

--- 354-russian-doll-envelopes/russian-doll-envelopes.cpp
+++ 354-russian-doll-envelopes/russian-doll-envelopes.cpp
@@ -1,26 +1,40 @@
 class Solution {
-public:
-    static bool comp(vector<int> &a,vector<int>&b){
-        if(a[0]==b[0]){
-            return a[1]>b[1];
+private:
+static bool cmp(vector<int> &a, vector<int> &b)
+    {
+        // If the widths are equal, sort in descending order of heights
+        if (a[0] == b[0])
+        {
+            return a[1] > b[1];
         }
-        else{
-            return a[0]<b[0];
-        }
+
+        // Sort in ascending order of widths
+        return a[0] < b[0];
     }
+
+int solveBS(int n,vector<vector<int>>& nums)
+    {
+            sort(nums.begin(),nums.end(),cmp);
+
+            vector<int> ans;
+            ans.push_back(nums[0][1]);
+
+            for(int i=1;i<n;i++)
+            {
+                if(nums[i][1]>ans.back()) ans.push_back(nums[i][1]);
+                else
+                {
+                    //find index of just bada element in ans
+                    int index=lower_bound(ans.begin(),ans.end(),nums[i][1])-ans.begin();
+                    ans[index]=nums[i][1];
+                }
+            }
+
+            return ans.size();
+    }       
+public:
     int maxEnvelopes(vector<vector<int>>& envelopes) {
-        sort(envelopes.begin(),envelopes.end(),comp);
-        vector<int> temp;
-        temp.push_back(envelopes[0][1]);
-        for(int i=1;i<envelopes.size();i++){
-            if(temp.back()<envelopes[i][1]){
-                temp.push_back(envelopes[i][1]);
-            }
-            else{
-                auto ind=lower_bound(temp.begin(),temp.end(),envelopes[i][1])-temp.begin();
-                temp[ind]=envelopes[i][1];
-            }
-        }
-        return temp.size();
+        int n=envelopes.size();
+        return solveBS(n,envelopes);      
     }
 };
