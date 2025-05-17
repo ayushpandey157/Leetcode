7b964ec02dfb622046fc0e3cc8b68de24fe05e80
ayushpandey157 <ayushpandey1572003@gmail.com>
2025-05-17 11:42:30 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (80.11%) - LeetSync

---
+++ 504-base-7/base-7.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    string convertToBase7(int num) {
+        if(num==0){
+            return "0";
+        }
+        string s;
+        int minus=0;
+        if(num<0){
+            minus=-1;
+            num*=-1;
+        }
+        while(num){
+            s+=(num%7)+'0';
+            num/=7;
+        }
+       
+        reverse(s.begin(),s.end());
+         if(minus==-1){
+            s="-"+s;
+        }
+        return s;
+    }
+};
