8a9706fa74a70f1bded6a514d4892f1c71c0e5ce
ayushpandey157 <ayushpandey1572003@gmail.com>
2025-05-17 12:49:41 +0530

    Time: 12 ms (31.67%) | Memory: 44.9 MB (43.16%) - LeetSync

---
+++ 455-assign-cookies/assign-cookies.cpp
@@ -0,0 +1,28 @@
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+class Solution {
+public:
+    int findContentChildren(vector<int>& g, vector<int>& s) {
+        sort(g.begin(), g.end()); 
+        sort(s.begin(), s.end());
+
+        int i = 0; // Pointer for children
+        int j = 0; // Pointer for cookies
+        int ans = 0;
+
+        while(i < g.size() && j < s.size()){
+            if(g[i] <= s[j]){
+                ans++;
+                i++;
+                j++;
+            } else {
+                j++;
+            }
+        }
+
+        return ans;
+    }
+};
